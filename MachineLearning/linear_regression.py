c9a3be8c4e5e25baf56b2a01273fac2346ec314f
unknown <liudanqing1996@hotmail.com>
2020-03-14 23:46:18 +0100

    linear regression to predict student grades

--- MachineLearning/linear_regression.py
+++ MachineLearning/linear_regression.py
@@ -1,2 +1,39 @@
-import tensorflow
-print("this works")
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
+import pickle
+from matplotlib import style
+
+data = pd.read_csv("student-mat.csv", sep=";")
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+
+predict = "G3"
+# delete the column that we want to predict
+X = np.array(data.drop(columns=[predict]))
+y = np.array(data[predict])
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train, y_train)
+acc = linear.score(x_test, y_test)
+print(acc)
+
+# Write the pickled representation of the object obj to the open file object file.
+# This is equivalent to Pickler(file, protocol).dump(obj).
+with open("studentmodel.pickle", "wb") as f:
+    pickle.dump(linear, f)
+    
+pickle_in = open("studentmodel.pickle", 'rb')
+linear = pickle.load(pickle_in)
+
+print("Co: \n", linear.coef_)
+print("Intercept \n", linear.intercept_)
+
+predictions = linear.predict(x_test)
+
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
