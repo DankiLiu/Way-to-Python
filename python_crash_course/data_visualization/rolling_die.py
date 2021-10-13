e547a7e2afeaff6771917f4567fb026279dfdc3a
Danki <liudanqing1996@hotmail.com>
2021-10-13 22:11:30 +0200

    Rolling dice

--- python_crash_course/data_visualization/rolling_die.py
+++ python_crash_course/data_visualization/rolling_die.py
@@ -1,3 +1,4 @@
+import pygal
 from die import Die
 
 
@@ -18,9 +19,41 @@ def analyse_results(die, results: []):
     for value in range(1, die.num_sides + 1):
         frequency = results.count(value)
         frequencies.append(frequency)
-    print(frequencies)
+    print("frequencies: ", frequencies)
+    # Visualize the result.
+    hist = pygal.Bar()
+    hist.title = "Result of rolling one D6 1000 times."
+    hist.x_labels = ['1', '2', '3', '4', '5', '6']
+    hist.x_title = "Result"
+    hist.y_title = "Frequency of Result"
+    hist.add('D6', frequencies)
+    hist.render_to_file('dis_visual.svg')
+
+
+def rolling_two_dice():
+    # Create two dice
+    die_1 = Die()
+    die_2 = Die()
+    # Make some rolls and store results in a list.
+    results = []
+    for roll_num in range(100):
+        result = die_1.roll() + die_2.roll()
+        results.append(result)
+    max_value = die_1.num_sides + die_2.num_sides
+    frequencies = []
+    for value in range(1, max_value + 1):
+        frequency = results.count(value)
+        frequencies.append(frequency)
+    print("frequencies:{}".format(frequencies))
+
+    hist = pygal.Bar()
+    hist.title = "Results of rolling two D6 dice 1000 times."
+    hist.x_labels = [str(i) for i in range(1, max_value + 1)]
+    hist.x_title = "Result"
+    hist.y_title = "Frequency of Result"
+    hist.add("D6", frequencies)
+    hist.render_to_file('dis_double_visual.svg')
 
 
 if __name__ == '__main__':
-    die, results = roll_die()
-    analyse_results(die, results)
+    rolling_two_dice()
